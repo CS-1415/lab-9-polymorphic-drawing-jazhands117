794f5fe9e05ccb8a140c161c295428442cae44b5
Jay Johnson <jay.johnson@students.snow.edu>
2025-11-05 12:59:21 -0700

    Final commit. Little guy printed.

--- Lab09.Main/obj/Debug/net8.0/Lab09.Main.AssemblyInfo.cs
+++ Lab09.Main/obj/Debug/net8.0/Lab09.Main.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab09.Main")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d09f29404749d6e663af09be832c1f2aff357e38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fae623bae81ec64a42514ea490d7d69f53254c69")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab09.Main")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab09.Main")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Lab09/obj/Debug/net8.0/Lab09.AssemblyInfo.cs
+++ Lab09/obj/Debug/net8.0/Lab09.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab09")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d3cf7537ee8bc82c04152fff4eeb4389acfbff4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fae623bae81ec64a42514ea490d7d69f53254c69")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab09")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab09")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
